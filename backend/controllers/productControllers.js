c9472599642e6a0714faadc35577fdd18b04add3
athulsebastiant <athulsebastiant.com>
2025-06-01 18:57:12 +0530

    created prod update and delete apis

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -9,6 +9,16 @@ export const getProducts = async (req, res) => {
   }
 };
 
+export const getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) return res.status(404).json({ message: "Product not found" });
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 export const createProduct = async (req, res) => {
   const {
     name,
@@ -42,5 +52,30 @@ export const createProduct = async (req, res) => {
     res.status(201).json(savedProduct);
   } catch (error) {
     res.status(400).json({ message: error.message });
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.json(updatedProduct);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+
+    if (!deletedProduct)
+      return res.status(404).json({ message: "Product Not Found" });
+    res.json({ message: "Product deleted succesfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 import * as productControllers from "../controllers/productControllers.js";
 
 router.get("/", productControllers.getProducts);
+router.get("/:id", productControllers.getProductById);
 router.post("/", productControllers.createProduct);
+router.put("/:id", productControllers.updateProduct);
+router.delete("/:id", productControllers.deleteProduct);
 
 export default router;
